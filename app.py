575828aafc52bc33cc0eb2b0487e231115275818
oianchevska <oianchevska@gmail.com>
2020-06-26 22:15:52 -0400

    PR-2: Added initial files v.3

--- app.py
+++ app.py
@@ -1,11 +1,12 @@
-from flask import Flask,render_template
+from flask import Flask, render_template
 
-app=Flask(__name__)
+app = Flask(__name__)
 
-@app.route('/')
+
+@app.route("/")
 def index():
     return render_template("index.html")
 
 
-if __name__=="__main__":
-    app.run()
+if __name__ == "__main__":
+    app.run(debug=True)
